912d4bdd5783b4977ac55fb16648be14ee752320
rdtricky <rndietrick@gmail.com>
2018-04-24 18:35:00 -0400

    removed console log statement

--- static/js/redux/actions/timetable_actions.jsx
+++ static/js/redux/actions/timetable_actions.jsx
@@ -538,7 +538,6 @@ export const fetchAdvisingTimetables = () => (dispatch, getState) => {
     .then((json) => {
       const { timetables, courses } = json;
       dispatch(receiveCourses(courses));
-      console.log(json);
       dispatch({
         type: ActionTypes.RECEIVE_ADVISING_TIMETABLES,
         timetables,
